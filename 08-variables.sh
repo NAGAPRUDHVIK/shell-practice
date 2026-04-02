02c519fb7106c1235baa27d049c89fcdbc0957e0
KNAGAPRUDHVI <knproyal.23@gmail.com>
2026-04-02 23:23:25 +0530

    Shell scripting practice

--- 08-variables.sh
+++ 08-variables.sh
@@ -6,4 +6,16 @@ echo "Fetches all names which is passed from arguments :$*"
 
 echo "To print script name:$0"
 
+echo "To print current working directory:$PWD"
+
+echo "To print the current user:$USER"
+
+echo "To print home directory:$HOME"
+
+echo "To print current PID:$$"
+
+sleep 10
+
+echo "To previous PID:$!"
+
 
