ed69b6568c77ebb07c262ceac713715d16625e73
KNAGAPRUDHVI <knproyal.23@gmail.com>
2026-04-02 23:38:25 +0530

    Removed commas

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -6,7 +6,7 @@ SUM=$((NUMBER1+NUMBER2+NUMBER3))
 
 echo "Total sum=$SUM" #Total sum will become 300. Beacuse that number3 will become as '0'. In shell script everything considered as a string.
 
-PLAYERS=("ViratKohli","KLR","SIRAJ")
+PLAYERS=("ViratKohli" "KLR" "SIRAJ")
 
 echo "All Players:${PLAYERS[@]}"
 
